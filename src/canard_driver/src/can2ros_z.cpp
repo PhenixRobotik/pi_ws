7fc24e9ee255c5a9792c01b250c375dfb75e1afc
Félix Piédallu <felix@piedallu.me>
2020-10-18 12:04:54 +0200

    Add all Z actions, and flaggy and arm

--- src/canard_driver/src/can2ros_z.cpp
+++ src/canard_driver/src/can2ros_z.cpp
@@ -5,9 +5,13 @@ std::map<CanardPortID, std::shared_ptr<CAN2ROS_interface>> can2ros_z;
 
 driver_data *pdata_ros_cb;
 
-auto z_valve= new CAN2ROS<std_msgs::Bool>    (can2ros_z, Z_VALVE_GET,   Z_VALVE_SET,"/z/valve");
 auto z_text = new CAN2ROS<std_msgs::String>  (can2ros_z, Z_TEXT_GET,    Z_TEXT_SET, "/z/text");
 auto z_pump = new CAN2ROS<std_msgs::Bool>    (can2ros_z, Z_PUMP_GET,    Z_PUMP_SET, "/z/pump");
+auto z_valve= new CAN2ROS<std_msgs::Bool>    (can2ros_z, Z_VALVE_GET,   Z_VALVE_SET,"/z/valve");
+auto z_zpos = new CAN2ROS<std_msgs::Bool>    (can2ros_z, Z_ZPOS_GET,    Z_ZPOS_SET, "/z/position");
+auto z_angle= new CAN2ROS<std_msgs::Bool>    (can2ros_z, Z_ANGLE_GET,   Z_ANGLE_SET,"/z/angle");
+auto flaggy = new CAN2ROS<std_msgs::Bool>    (can2ros_z, FLAGGY_GET,    FLAGGY_SET, "/flaggy");
+auto arm    = new CAN2ROS<std_msgs::Bool>    (can2ros_z, ARM_GET,       ARM_SET,    "/arm");
 
 void init_subscription_z(driver_data *pdata){
     pdata_ros_cb = pdata;
